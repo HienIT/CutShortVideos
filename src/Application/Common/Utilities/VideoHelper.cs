9d5f0776633b52c3851d800df65d9d1f586cb9c3
tronghien.it <tronghien.it@gmail.com>
2024-04-18 00:02:15 +0700

    Commit CreateRawVideoCommand

---
+++ src/Application/Common/Utilities/VideoHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Common.Utilities;
+public static class VideoHelper
+{
+    public static (TimeSpan, string) GetVideoInfo(string videoPath)
+    {
+        // Lấy thông tin thẻ
+        var tag = TagLib.File.Create(videoPath);
+
+        // Lấy thời gian
+        var duration = tag.Properties.Duration;
+
+        // Lấy độ phân giải
+        var width = tag.Properties.VideoWidth;
+        var height = tag.Properties.VideoHeight;
+
+        return (duration, $"{width}x{height}");
+    }
+}
---
+++ src/Application/RawVideos/Commands/CreateRawVideo/CreateRawVideo.cs
@@ -0,0 +1,58 @@
+﻿using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Common.Utilities;
+using CleanArchitecture.Application.TodoItems.Commands.UpdateTodoItemDetail;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.RawVideos.Commands.CreateRawVideo;
+public record CreateRawVideoCommand : IRequest
+{
+    public required string Directory { get; init; }
+    public string CreateUser { get; init; } = "admin";
+    public string UpdateUser { get; init; } = "admin";
+}
+
+public class CreateRawVideoCommandHandler : IRequestHandler<CreateRawVideoCommand>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateRawVideoCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(CreateRawVideoCommand request, CancellationToken cancellationToken)
+    {
+        var rawVideos = _context.RawVideos.Where(m => m.Isdelete != true).ToList();
+        var listFileByDirectory = Directory.GetFiles(request.Directory);
+
+        foreach (var file in listFileByDirectory)
+        {
+            FileInfo fi = new FileInfo(file);
+
+            if (rawVideos.Exists(m => m.Filename == fi.Name && m.Directory == request.Directory))
+            {
+                continue;
+            }
+
+            var videoInfo = VideoHelper.GetVideoInfo(fi.FullName);
+
+            RawVideo newItem = new RawVideo()
+            {
+                Directory = request.Directory,
+                Filename = fi.Name,
+                Rawvideoname = fi.Name,
+                Ext = fi.Extension,
+                Durationtime = videoInfo.Item1.TotalMinutes,
+                Resolution = videoInfo.Item2,
+                Createdate = DateTime.Now,
+                Updatedate = DateTime.Now,
+                Createuser = request.CreateUser,
+                Updateuser = request.UpdateUser
+            };
+
+            _context.RawVideos.Add(newItem);
+        }
+
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
